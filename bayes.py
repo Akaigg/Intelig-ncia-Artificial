cd688a8e00b1e5d97f04d3520ff423117ee8bca0
Vitor Blume <124194664+Akaigg@users.noreply.github.com>
2025-04-10 20:02:06 -0300

    Update bayes.py

--- bayes.py
+++ bayes.py
@@ -10,11 +10,20 @@ def extract_files(uploader):
             for page in doc:
                 text += page.get_text("text") 
     return text
-    
+
+def chat_with_groq(prompt, context):
+    response = client.chat.completions.create(
+        model = "llama-3.3-70b-versatile",
+        message = [
+            {"role": "system", "content": "Voce e um assistente que reponde os dados do pdf e ajuda na minha rotina de estudos"},
+            {"role": "user", "content": f"{context}\n\nPergunta: {prompt}}
+        ]
+    )
+    return response.choice[0].message.content
     
 # CRIAR A INTERFACE
 def main():
-    st.title("O nome do meu sistema inteligente")
+    st.title("Bagulho de organização de estudos")
     # Incluir uma imagem de acordo ao sistema escolhido
     with st.sidebar:
         st.header("UPLoader Files")
@@ -23,8 +32,7 @@ def main():
         text = extract_files(uploader)
         st.session_state["document-text"] = text
     user_input = st.text_input("Digite a sua pergunta")
-
-
+    chat_with_groq(user_input, text)
 
 if __name__ == "__main__":
     main()
